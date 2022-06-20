52fdf9f0bf201471b9d21bbfdd6cb02d130c0f1f
ekaterina <katerina_w@icloud.com>
2022-06-20 23:37:07 +0300

    Задачи и дз к занятию 2

---
+++ task_08/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+78 -> 8 12-> 2 85 -> 8
+10 99*/
+ 
+int num = new Random().Next(10,100);
+
+Console.WriteLine(num);
+
+int c1 = num / 10;
+int c2 = num % 10;
+
+if(c1<c2)
+{
+    Console.WriteLine (c2);
+}
+else 
+{
+    Console.WriteLine (c1);
+}
---
+++ task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью 
+цифру заданного числа
+ или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число"); 
+int x = int.Parse(Console.ReadLine()); 
+
+if (x < 100)
+{
+    Console.WriteLine("Нет третьего числа");
+}
+else 
+{ 
+    while (x >= 1000){x = x / 10;}
+}
+int num3 = x % 10;
+Console.WriteLine(num3);
